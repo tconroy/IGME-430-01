532a2c5281bdc6bdc62aedec989ae670dd8d939e
Tom Conroy <tdc5536@rit.edu>
2014-09-10 19:39:26 -0400

    console verisons of TCP and WS. chat works, need to add user list settings and termination handling.

---
+++ Homework/1 - C# Chat Client/TCPChat/ConsoleMessageClient/ClientTCP.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//import out network packages
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleMessageClient
+{
+    //enum to hold message types. These will be used to distinguish different types of messages.
+    public enum MessageType
+    {
+        Joined,
+        Left,
+        Message
+    }
+
+    class ClientTCP
+    {
+        //tcpclient object for our tcp connection/methods
+        private TcpClient tcpClient;
+
+        string username = "";
+
+        //network stream for communicating with the server
+        NetworkStream tcpStream;
+
+        //startup method
+        public void startup()
+        {
+            //On a ctrl+c command, this event will fire and shut down
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(shutdown);
+
+            //string to store the server's IP
+            string ip;
+
+            //grab username from console
+            while (username == "")
+            {
+                Console.Write("\nEnter username: ");
+
+                username = Console.ReadLine();
+            }
+
+
+            try
+            {
+                //read IP from command line
+                Console.Write("Enter IP of chat server: ");
+                ip = Console.ReadLine();
+
+                //Create a new IPEndpoint to the server for port 8012. 
+                IPEndPoint tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), 8012);
+
+                //create a new tcpclient and connect to our endpoint.
+                //Connecting will start the stream or throw an exception
+                tcpClient = new TcpClient();
+                tcpClient.Connect(tcpEndPoint);
+
+                //get the newly created stream from the connected tcpclient object
+                tcpStream = tcpClient.GetStream();
+
+                //send joined message
+                sendTCPData(username, MessageType.Joined, "");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not connect to server");
+                System.Environment.Exit(-1);
+            }
+
+            //start new task to listen for new TCP data
+            //the code will move on and not wait for this because it is a task
+            Task.Factory.StartNew(() =>
+            {
+                getTCPData();
+            });
+
+            Console.Write("\nEnter Message and hit enter to send\n");
+
+            //keep accepting console input
+            while (true)
+            {
+                //get string from the console
+                string text = Console.ReadLine();
+
+                //if they actually typed a message
+                if (text != "")
+                {
+                    //send a message to the server
+                    sendTCPData(username, MessageType.Message, text);
+                }
+            }
+        }
+
+        /**
+         * Only fires on ctrl-C events
+         * */
+        public void shutdown(object sender, ConsoleCancelEventArgs args)
+        {
+            sendTCPData(username, MessageType.Left, "");
+        }
+
+        //method to receive data from the server
+        void getTCPData()
+        {
+            //new byte array to hold data. We don't know the size of the data, so 1024 gives us a decent amount of room
+            byte[] buffer = new byte[1024];
+            //int to hold the number of bytes we read
+            int bytes = 0;
+
+            //strings to hold the username and message we received from the server
+            string message = "";
+            string messageUser = "defaultUserName";
+
+            //read 1 byte from the stream starting at position 0 of the stream and put it into the buffer
+            //The return is the number of bytes we read from the stream. This will be 0 if the stream is empty
+            //If the stream has data the first byte will be put into our buffer array
+            bytes = tcpStream.Read(buffer, 0, 1);
+
+            //if we read any bytes above- meaning the stream was not empty
+            if (bytes != 0)
+            {
+                //read from the stream starting at position 1 and read 2 spots and add them to the buffer.
+                //this will read array positions [1] and [2] from the stream.
+                //again, the return will be the number of bytes actually read. 
+                bytes = tcpStream.Read(buffer, 1, 2);
+                //now that we filled the buffer some more, read out the username length
+                //we know [1] is the modulus of 256 and [2] is the number of times the username goes into 256
+                short usernameLength = (short)(buffer[1] + (buffer[2] * 256));
+
+                //read how long the message is starting at position [3] from the stream and reading 1
+                //this will append to the buffer again
+                bytes = tcpStream.Read(buffer, 3, 1);
+                //grab the message length from the newly filled buffer
+                short messageLength = (short)(buffer[3]);
+
+                //read out the username from the stream starting at [4] and going the username length.
+                //this will be pulled into the buffer
+                bytes = tcpStream.Read(buffer, 4, usernameLength);
+                //read the username from the buffer now that we read it into the buffer
+                messageUser = Encoding.ASCII.GetString(buffer, 4, usernameLength);
+
+                //if the message type is message
+                if (buffer[0] == (byte)MessageType.Message)
+                {
+                    //read message into buffer
+                    bytes = tcpStream.Read(buffer, 4 + usernameLength, messageLength);
+                    //get the message from the buffer
+                    message = Encoding.ASCII.GetString(buffer, 4 + usernameLength, messageLength);
+
+                    Console.Write("\n" + messageUser + ":" + message + "\n");
+                }
+            }
+
+            //start a new task to start listening for more data again
+            Task.Factory.StartNew(() =>
+            {
+                getTCPData();
+            });
+
+        }
+
+        //method to send tcp data
+        void sendTCPData(string username, MessageType type, string message)
+        {
+            //byte array to hold our parsing information - how long usernames are, how long messages are, etc
+            byte[] packetBuffer = new byte[4];
+            //byte array formed from the ascii byte version of our username
+            byte[] usernameBuffer = System.Text.Encoding.ASCII.GetBytes(username);
+            //byte array formed from the ascii byte version of our message
+            byte[] messageBuffer = System.Text.Encoding.ASCII.GetBytes(message);
+
+            //The first 4 bytes will be our parsing information. This is how we will know how long each section of the data is when it arrives at the server
+            //The server will read these to figure out how many bytes are in each part of the data
+            packetBuffer[0] = (byte)type;
+            packetBuffer[1] = (byte)(usernameBuffer.Length % 256);
+            packetBuffer[2] = (byte)(usernameBuffer.Length / 256);
+            packetBuffer[3] = (byte)(messageBuffer.Length);
+
+            //concat the 3 byte arrays into one (make sure they are in the right order)
+            byte[] tcpMessage = packetBuffer.Concat(usernameBuffer).Concat(messageBuffer).ToArray();
+
+            //send to our client. 
+            //in a chat server, you would send to every client
+            tcpClient.Client.Send(tcpMessage);
+        }
+
+    }
+}
---
+++ Homework/1 - C# Chat Client/TCPChat/ConsoleMessageServer/ServerTCP.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//import our network packages
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleMessageServer
+{
+    //enum to hold message types. These will be used to distinguish different types of messages.
+    public enum MessageType
+    {
+        Joined,
+        Left,
+        Message
+    }
+
+
+    class ServerTCP
+    {
+        //Our server's TCP socket
+        Socket tcpSocket;
+        //list to hold all of our clients. These will be of a custom clientmanger class we will build
+        List<ClientManager> tcpClients = new List<ClientManager>();
+
+        //the last client that connected. We will only use this for the example. 
+        //YOUR chat server will not have this
+        //ClientManager lastClient;
+
+        //startup method
+        public void startup()
+        {
+            //new TCP task to run asynchronously in the background
+            Task.Factory.StartNew(() =>
+            {
+                //listen to any address on port 8080
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8012);
+                //set the socket type to a stream of protocol tcp
+                tcpSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                //bind to the endpoint to the socket
+                tcpSocket.Bind(localEndPoint);
+                //start listening and allowing queuing of up to 10 connections. If the system is overwhelming and the queue goes over 10, the later ones will be dropped
+                tcpSocket.Listen(10);
+
+                //begin accepting connections and call onAccept when a new client connects. 
+                tcpSocket.BeginAccept(new AsyncCallback(OnAccept), null);
+
+            });
+
+            Console.WriteLine("Hit ctrl-c twice to exit");
+
+            //only here to keep the console open
+            Console.ReadKey();
+        }
+
+        //Called when a new client starts a tcp connection to the server
+        void OnAccept(IAsyncResult connection)
+        {
+            //finish accepting and grab socket from connection
+            Socket clientSocket = tcpSocket.EndAccept(connection);
+            //pass socket to our clientmanager class. This will handle all of the reads/writes for this stream
+            ClientManager client = new ClientManager(clientSocket);
+
+            //add a reference to our last connected client for a response
+            //YOU will not have this in your chat server because you will respond to everyone
+            //lastClient = client;
+            if( ! tcpClients.Contains(client) ){
+                tcpClients.Add(client);
+            }
+
+            //connect the clientmanager's onleft event to the onleft method in this server
+            client.OnLeft += OnLeft;
+            //connect the clientmanager's onmessage event to the onmessage method in this server
+            client.OnMessage += OnMessage;
+            //connect the clientmanager's onjoined event to the onjoined method in this server
+            client.OnJoined += OnJoined;
+
+            //add the client to our TCP clients list
+            //tcpClients.Add(client);
+
+            //begin accepting more connections
+            tcpSocket.BeginAccept(new AsyncCallback(OnAccept), null);
+        }
+
+        //method will be called every time a clientmanager onleft event fires
+        //will be called when clients send a left message
+        void OnLeft(ClientManager item)
+        {
+            //remove the client from our TCP clients list
+            tcpClients.Remove(item);
+            //disconnect the client and discard the socket. False means that the socket cannot be used again. 
+            //True means you can keep using the socket methods even though it's disconnected
+            item.socket.Disconnect(false);
+        }
+
+        //method will be called every time a clientmanager onjoined event fires
+        //will be called when clients send a joined message
+        void OnJoined(string username)
+        {
+            sendChatMessage(username, " has joined chat");
+        }
+
+        void OnMessage(string username, string message)
+        {
+            sendChatMessage(username, message);
+        }
+
+        //method to send chat messages to clients
+        void sendChatMessage(string username, string message)
+        {
+            //byte array to hold our parsing information - how long usernames are, how long messages are, etc
+            byte[] packetBuffer = new byte[4];
+            //byte array formed from the ascii byte version of our username
+            byte[] usernameBuffer = System.Text.Encoding.ASCII.GetBytes(username);
+            //byte array formed from the ascii byte version of our message
+            byte[] messageBuffer = System.Text.Encoding.ASCII.GetBytes(message);
+
+            //The first 4 bytes will be our parsing information. This is how we will know how long each section of the data is when it arrives at the server
+            //The server will read these to figure out how many bytes are in each part of the data
+            // [0] will be the message type
+            // [1] will be the remainder of the username length % 256.
+            // [2] will be how many types the username goes into 256. 
+            // [3] will be the length of the message
+            packetBuffer[0] = (byte)MessageType.Message;
+            packetBuffer[1] = (byte)(usernameBuffer.Length % 256);
+            packetBuffer[2] = (byte)(usernameBuffer.Length / 256);
+            packetBuffer[3] = (byte)(messageBuffer.Length);
+
+            //concat the 3 byte arrays into one (make sure they are in the right order)
+            byte[] tcpMessage = packetBuffer.Concat(usernameBuffer).Concat(messageBuffer).ToArray();
+
+            //send our byte array
+            //lastClient.socket.Send(tcpMessage);
+
+            foreach( ClientManager client in tcpClients ){
+                client.socket.Send(tcpMessage);
+            }
+
+        }
+
+    }
+}
